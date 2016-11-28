eb3963563ee197a1d29731f8a1261ef3efd6a8ed
Furkan <furkan.demirci@live.nl>
2016-11-28 15:05:28 +0100

    Footer verbeterd
    betere informatie en ook brandlogo redcloaks bij toegevoegd.

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,33 +1,31 @@
 <div class="container">
   <div class="row">
     <div class="col l6 s12">
-      <h5 class="white-text">Redcloaks</h5>
-      <p class="grey-text text-lighten-4">Wij zijn studenten van het Roc Ter AA applicatie ontwikkelaar. We hebben een bedrijf genaamd Redcloaks.</p>
+      <h5 class="white-text">Over ons:</h5>
+      <p class="grey-text text-lighten-4">
 
+      Wij zijn studenten van het Roc Ter AA applicatie ontwikkelaar. We hebben aan deze opdracht gewerkt als proftaak. 
+      Ontwikkelaars: Furkan Demirci en Yaime van der Vleuten.
+      </p>
 
+      <img class="brandlogo" src="{{ URL::asset ('img/brandlogo.png') }}">
     </div>
     <div class="col l3 s12">
       <h5 class="white-text">Settings</h5>
       <ul>
         <li><a class="white-text" href="#!">Link 1</a></li>
-        <li><a class="white-text" href="#!">Link 2</a></li>
-        <li><a class="white-text" href="#!">Link 3</a></li>
-        <li><a class="white-text" href="#!">Link 4</a></li>
       </ul>
     </div>
     <div class="col l3 s12">
       <h5 class="white-text">Connect</h5>
       <ul>
         <li><a class="white-text" href="#!">Link 1</a></li>
-        <li><a class="white-text" href="#!">Link 2</a></li>
-        <li><a class="white-text" href="#!">Link 3</a></li>
-        <li><a class="white-text" href="#!">Link 4</a></li>
       </ul>
     </div>
   </div>
 </div>
 <div class="footer-copyright">
   <div class="container">
-  Made by <a class="orange-text text-lighten-3" href="http://materializecss.com">Materialize</a>
+  Made by <a class="red-text text-lighten-3" href="#">Redcloaks</a>
   </div>
 </div>
