2f80c22ff9572230b48aae33375414afbb322ac8
Furkan <furkan.demirci@live.nl>
2016-12-06 01:42:05 +0100

    Sidebar
    Beter en functionerend gemaakt

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -1,11 +1,11 @@
 <div class="navbar">
     <nav class="teal lighten-1" role="navigation">
         <div class="nav-wrapper container"><a id="logo-container" href="../index.php" class="brand-logo"><b>ROC Ter
-                    AA</b> <i>studiehulp</i></a>
+                    AA</b> <i id="sidebarText">studiehulp</i></a>
             <ul class="right hide-on-med-and-down">
                 <li><a href="../index.php">Home</a></li>
-                <li><a href="../tabs/student.php">Test1</a></li>
-                <li><a href="../tabs/begeleider.php">Test2</a></li>
+                <li><a href="../tabs/student.php">Student</a></li>
+                <li><a href="../tabs/begeleider.php">Begeleider</a></li>
                 <li><a class="teal darken-1 waves-effect waves-light btn" href="../tabs/login.php">Login</a></li>
             </ul>
 
@@ -20,13 +20,18 @@
                         <a href="#"><span class="white-text email">Admin@test.test</span></a>
                     </div>
                 </li>
-                <li><a href="#"><i class="material-icons">cloud</i>First Link With Icon</a></li>
-                <li><a href="#">Second Link</a></li>
+                <li><a class="waves-effect" href="../index.php"><i class="material-icons">store</i>Home</a></li>
+                <li><a class="waves-effect" href="../tabs/student.php"><i class="material-icons">subject</i>Student</a>
+                </li>
+                <li><a class="waves-effect" href="../tabs/begeleider.php"><i class="material-icons">assignment</i>Begeleider</a>
+                </li>
                 <li>
                     <div class="divider"></div>
                 </li>
-                <li><a class="subheader">Subheader</a></li>
-                <li><a class="waves-effect" href="#">Third Link With Waves</a></li>
+                <li><a class="subheader">Account</a></li>
+                <li><a class="waves-effect" href="../tabs/login.php"><i class="material-icons">supervisor_account</i>Login
+                        / Registreren</a>
+                </li>
             </ul>
             <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
         </div>
